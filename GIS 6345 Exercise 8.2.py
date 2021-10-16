c47369ce55e1355606d5e873ce74476178469fdd
NanaArthur21 <91926412+NanaArthur21@users.noreply.github.com>
2021-10-15 20:56:31 -0500

    Add files via upload

---
+++ GIS 6345 Exercise 8.2.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+word = 'banana'
+
+
+# In[2]:
+
+
+count = 0
+
+
+# In[3]:
+
+
+for letter in word:
+    if letter == 'a':
+        count = count + 1
+
+
+# In[4]:
+
+
+print(count)
+
+
+# In[5]:
+
+
+word = 'hippopotamus'
+
+
+# In[6]:
+
+
+count = 0
+
+
+# In[7]:
+
+
+for letter in word:
+    if letter == 'o':
+        count = count + 1
+
+
+# In[8]:
+
+
+print(count)
+
+
+# In[9]:
+
+
+word = 'hippopotamus'
+
+
+# In[10]:
+
+
+count = 0
+
+
+# In[11]:
+
+
+for letter in word:
+    if letter == 'a':
+        count = count + 1
+
+
+# In[12]:
+
+
+print(count)
+
+
+# In[13]:
+
+
+word = 'hippopotamus'
+
+
+# In[14]:
+
+
+count = 0
+
+
+# In[15]:
+
+
+for letter in word:
+    if letter == 'p':
+        count = count + 1
+
+
+# In[16]:
+
+
+print(count)
+
+
+# In[ ]:
+
+
+
+
